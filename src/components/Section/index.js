ef52253e6b0dffaa5f3ebc9c5f4bad827c3ac857
Gustavo Silva <gustagui.dev@gmail.com>
2021-11-23 22:18:29 -0300

    add routes

--- src/components/Section/index.js
+++ src/components/Section/index.js
@@ -1,6 +1,16 @@
 import './style.css'
 
-export default function Section({title, description, image, alterna}){    
+export default function Section({title, description, image, alterna}){
+  
+    function zoom(event) {
+      event.target.style.cursor = 'pointer'
+      event.target.style.transform = 'scale(1.5)'
+    }
+
+    function outZoom(event) {
+      event.target.style.transform = 'scale(1)'
+    }
+  
     return (
       <section className={`motivos ${alterna && 'alterna'} `}>
         <div>
@@ -8,11 +18,8 @@ export default function Section({title, description, image, alterna}){
           <p>{description}</p>
         </div>
         <div>
-          <img src={image} alt="Motivo 1" />
+          <img src={image} alt="Motivo 1" onMouseMove={zoom} onMouseOut={outZoom} />
         </div>
       </section>
-    )
+    );
 }
-
-
-
---
+++ src/pages/Home/index.js
@@ -0,0 +1,78 @@
+import './app.css'
+import './netflix.css'
+
+import Logo from '../../assets/images/logo.png'
+import Motivo1 from '../../assets/images/motivo-1.png'
+import Motivo2 from '../../assets/images/motivo-2.png'
+import Button from '../../components/Button'
+import Section from '../../components/Section'
+import MOTIVOS from '../../constants/motivos'
+
+import { Link } from 'react-router-dom'
+
+export default function Home() {
+  const idioma = 'PT';
+
+  const ola = () => {
+    alert('Oi, tudo bom?')
+  }
+
+  return (    
+    <div>
+      <header>
+        <div>
+          <img src={Logo} alt="Logo" />
+          <Link to="/about">Link pra About</Link>
+        </div>
+        <div id="login">
+          <select id="idioma">
+            <option value="PT">Português</option>
+            <option value="ING">Ingles</option>
+          </select>
+          <Button variante="sucesso" text='Entrar' />
+        </div>
+      </header>
+      <section id="banner">
+        <h1>
+          Filmes, séries e muito <br />
+          mais. Sem limites.
+        </h1>
+        <h3>Assista onde quiser. Cancele quando quiser.</h3>
+        <small>
+          Pronto para assistir? Informe seu email para criar ou reiniciar sua
+          assinatura.
+        </small>
+        <div>
+          <input type="email" placeholder="Email" />
+          <Button variante="alerta" text= 'Vamos lá'/>
+        </div>
+      </section>
+      <hr className="divisor" />
+
+      <Section title={MOTIVOS[idioma].ONE.TITLE} 
+            description={MOTIVOS[idioma].ONE.DESCRIPTION}
+            image={Motivo1} />
+
+      <hr className="divisor" />
+
+      <Section title={MOTIVOS[idioma].ONE.TITLE}
+            description={MOTIVOS[idioma].ONE.DESCRIPTION} 
+            image={Motivo2} alterna={true} />
+
+      <hr className="divisor" />
+
+      <Section title={MOTIVOS[idioma].ONE.TITLE}
+            description={MOTIVOS[idioma].ONE.DESCRIPTION} 
+            image={Motivo2}  />
+
+      <hr className="divisor" />
+
+      <Section title={MOTIVOS[idioma].ONE.TITLE}
+            description={MOTIVOS[idioma].ONE.DESCRIPTION} 
+            image={Motivo2} alterna={true} />
+
+      <hr className="divisor" />
+      
+    </div>
+  );
+}
